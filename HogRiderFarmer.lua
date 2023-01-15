7505dc7c9805018e8e6d05e9bbcf0639416a1e09
nunera <67439647+nunera@users.noreply.github.com>
2023-01-14 20:17:58 -0600

    Create HogRiderFarmer.lua

---
+++ HogRiderFarmer.lua
@@ -0,0 +1,85 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Hog Rider Farmer", HidePremium = true, SaveConfig = true, ConfigFolder = "HogRiderFarmer",IntroText = "Hog Rider Farmer Loading..."})
+local plr = game.Players.LocalPlayer.Name
+local del = .8
+local bdel = .1
+local hdel = .1
+local Farmer = Window:MakeTab({
+    Name = "Autofarm",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+_G.heh = false
+local farm = Farmer:AddToggle({
+    Name = "Farm",
+    Default = false,
+    Callback = function(we)
+        if we then
+            _G.heh = true
+            while _G.heh do 
+                workspace.Characters[plr].CharacterHandler.M1:FireServer("Down", false)
+                task.wait(del)
+                workspace.Characters[plr].CharacterHandler.F:FireServer("Down")
+                task.wait(hdel)
+                workspace.Characters[plr].CharacterHandler.F:FireServer("Up")
+                task.wait(bdel)
+            end
+        else
+            _G.heh = false
+        end
+    end
+})
+
+local bind = Farmer:AddBind({
+    Name = "Keybind",
+    Default = "",
+    Hold = false,
+    Callback = function()
+        if _G.heh then
+            farm:Set(false)
+        else
+            farm:Set(true)
+        end
+    end
+})
+
+local hdelay = Farmer:AddSlider({
+    Name = "Hit Delay",
+    Min = 0,
+    Max = 3,
+    Default = .8,
+    Color = Color3.fromRGB(75,75,75),
+    Increment = .1,
+    ValueName = "Seconds",
+    Callback = function(val)
+        del = val
+    end
+})
+
+local bdelay = Farmer:AddSlider({
+    Name = "Block Delay",
+    Min = 0,
+    Max = 3,
+    Default = .1,
+    Color = Color3.fromRGB(120,80,120),
+    Increment = .1,
+    ValueName = "Seconds",
+    Callback = function(val)
+        bdel = val
+    end
+})
+
+local bdelay = Farmer:AddSlider({
+    Name = "Block Delay",
+    Min = 0,
+    Max = 1,
+    Default = .1,
+    Color = Color3.fromRGB(225,80,90),
+    Increment = .01,
+    ValueName = "Seconds",
+    Callback = function(val)
+        hdel = val
+    end
+})
+
+OrionLib:Init()
